26e9c37143d4092b0686feae7c7fc170a21dfad9
shafayeatt1995 <shafayetalanik@gmail.com>
2024-12-12 02:35:14 +0600

    xml updated

--- src/app/sitemap.xml/route.js
+++ src/app/sitemap.xml/route.js
@@ -14,8 +14,7 @@ export async function GET() {
   const allRoutes = [...staticRoutes, ...dynamicRoutes];
 
   // Generate sitemap XML
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
     ${allRoutes
       .map(
         (route) => `
